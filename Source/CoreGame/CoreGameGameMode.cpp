0627ad100b00bc7c5ce1f0736da8a9d47c4afb95
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2024-02-03 13:23:14 +0100

    Fix paths

--- Source/CoreGame/CoreGameGameMode.cpp
+++ Source/CoreGame/CoreGameGameMode.cpp
@@ -7,7 +7,7 @@
 ACoreGameGameMode::ACoreGameGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/3rdPartyAssets/packages/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
