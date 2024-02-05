a6a347343b7978d5c717d3bc22eadb793904f589
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2024-02-05 15:19:02 +0100

    WIP Git update

--- Source/CoreGame/CoreGameCharacter.cpp
+++ Source/CoreGame/CoreGameCharacter.cpp
@@ -10,7 +10,7 @@
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "InputActionValue.h"
-#include "Actors/TCBomb.h"
+#include "RoundComponents/TCBomb.h"
 
 DEFINE_LOG_CATEGORY(LogTemplateCharacter);
 
